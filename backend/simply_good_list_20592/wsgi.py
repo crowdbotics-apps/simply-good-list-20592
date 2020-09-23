538cc0f89e2828eafcb3922a308b3841e2081534
Crowdbotics <team@crowdbotics.com>
2020-09-23 03:57:58 +0000

    Update models

--- backend/simply_good_list_20592/wsgi.py
+++ backend/simply_good_list_20592/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simply_good_list_20592.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simply_good_list_20592.settings")
 
 application = get_wsgi_application()
